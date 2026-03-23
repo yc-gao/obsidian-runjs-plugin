8aaf49f760e3a43df85ef94d2c0756d96e0c1370
ycgao <yc_x@outlook.com>
2026-03-23 16:19:28 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
 import { Plugin } from 'obsidian';
 
+import * as echarts from 'echarts';
+
 export default class Main extends Plugin {
+    echarts: typeof echarts;
+
+    constructor(app: any, manifest: any) {
+        super(app, manifest);
+        this.echarts = echarts;
+    }
+
     async onload() {
         console.log('Loading plugin: ' + this.manifest.name);
 
-        this.registerMarkdownCodeBlockProcessor('runjs', (source, el, ctx) => {
-            this.renderJs(source, el);
+        this.registerMarkdownCodeBlockProcessor('runjs', async (source, el, ctx) => {
+            await this.renderJs(source, el);
         });
     }
 
-    async renderJs(source: string, el: HTMLElement) {
-        (0, eval)(source);
+    async renderJs(source: string, el: HTMLElement): Promise<void> {
+        eval(source);
     }
 
     onunload() {
